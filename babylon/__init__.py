62db19589571541f617da22f88041756f2e8ca2d
James Cleveland <jamescleveland@gmail.com>
2012-09-04 13:40:21 +0100

    made vastly more flexible

--- babylon/__init__.py
+++ babylon/__init__.py
@@ -1,24 +1,36 @@
 import hashlib
+import abc
 
 from django.core.cache import cache as django_cache
 from django.db.models.signals import post_save
 
 CACHES = {}
 
-def register(cache):
+def register(cache, parents=None):
     """Register a cache so it can be used."""
     if not hasattr(CACHES, cache.__name__):
         CACHES[cache.__name__] = cache(CACHES)
+    cache = CACHES[cache.__name__]
+    if parents:
+        for parent in parents:
+            CACHES[parent].add_child(cache)
 
-def get(cache, instance, *args, **kwargs):
+def get(cache, *args, **kwargs):
     """Get a cache with a certain primary instance."""
     if not str(cache):
         cache = cache.__name__
-    return CACHES[cache].get(instance, *args, **kwargs)
+    return CACHES[cache].get(*args, **kwargs)
+
+
+def register_child(cache, child):
+    child = CACHES[child]
+    CACHES[cache].add_child(child)
 
 
 class Cache(object):
     """This is the class from which to inherit from."""
+    __metaclass__ = abc.ABCMeta
+
     model = None
     dependencies = []
     hooks = ()
@@ -35,39 +47,59 @@ class Cache(object):
                 caches[cls] = instance
             else:
                 instance = caches[cls]
-            self._children[cls] = instance
-            instance._parents.append(self)
+            self.add_child(instance)    
         post_save.connect(self.invalidate, sender=self.model)
 
         self._hooks()
 
-    def key(self, instance, *args, **kwargs):
-        key = '{}:{}'.format(self.__class__.__name__, instance.pk)
+    def add_child(self, child):
+        self._children[child.__class__] = child
+        child._parents.append(self)
+
+    def key(self, *args, **kwargs):
+        key = '{}'.format(self.__class__.__name__)
+        for arg in args:
+            if hasattr(arg, '__key__'):
+                ikey = arg.__key__()
+            elif hasattr(arg, 'id'):
+                ikey = arg.id
+            else:
+                ikey = arg
+            
+            key += ':{}'.format(ikey)
         return hashlib.sha1(key).hexdigest()
 
     def child(self, child, instance):
         return self._children[child].get(instance)
 
-    def get(self, instance, *args, **kwargs):
-        result = django_cache.get(self.key(instance, *args, **kwargs))
+    def get(self, *args, **kwargs):
+        result = django_cache.get(self.key(*args, **kwargs))
         if not result:
-            result = self.generate(instance, *args, **kwargs)
-            self.set(instance, result, *args, **kwargs)
+            result = self.generate(*args, **kwargs)
+            self.set(result, *args, **kwargs)
         return result
 
-    def set(self, instance, data, *args, **kwargs):
-        django_cache.set(self.key(instance, *args, **kwargs),
+    def set(self, data, *args, **kwargs):
+        django_cache.set(self.key(*args, **kwargs),
             data, Cache.TIMEOUT)
 
     def _hooks(self):
         for model, func in self.hooks:
             post_save.connect(getattr(self, func), sender=model)
 
-    def invalidate(self, sender, instance, **kwargs):
-       self.set(instance, self.generate(instance))
-       for parent in self._parents:
-            parent.invalidate(instance)
+    def invalidate(self, sender, instance=None, *args, **kwargs):
+        """This invalidates the cache and it's parents, and calls the
+        regenerate method for all of them."""
+        if instance:
+            self.set(instance, self.generate(instance))
+        else:
+            for obj in self.model.objects.all():
+                self.invalidate(sender, instance=obj)
+        for parent in self._parents:
+            parent.invalidate(sender, *args, **kwargs)
 
+
+    @abc.abstractmethod
     def generate(self, instance):
-        raise Exception('You MUST override this method and return the data '
-            + 'that this cache represents!')
+        """This method should return up to date contents of the cache."""
+        return
